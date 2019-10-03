6ee6dc12c098c75e9742109190c6914c59e73a14
Krzysztof Pietruszka <pietruszkak@gmail.com>
2019-10-03 20:12:34 +0200

    PropTypes added

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,9 @@
 import React from "react";
 import "./Input.css";
+import PropTypes from "prop-types";
 
 export const Input = props => <div className="input">{props.input}</div>;
+
+Input.propTypes = {
+  input: PropTypes.string.isRequired
+};
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,4 +1,9 @@
 import React from "react";
 import "./Result.css";
+import PropTypes from "prop-types";
 
 export const Result = props => <div className="result">{props.result}</div>;
+
+Result.propTypes = {
+  result: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
